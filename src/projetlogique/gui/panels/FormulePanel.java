9dfa9d83081b466aa08f7a40bf756e8b787d786d
Mathis-Capisano <mathis.capisano@etu.umontpellier.fr>
2020-12-02 11:23:05 +0100

    Modification de l'affichage pour l'arbre + fix de bugs d'affichages

--- src/projetlogique/gui/panels/FormulePanel.java
+++ src/projetlogique/gui/panels/FormulePanel.java
@@ -1,25 +1,33 @@
 package projetlogique.gui.panels;
 
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JLayeredPane;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
+import projetlogique.formules.Formule;
+
 public class FormulePanel extends JPanel {
 	
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 6335221551069472985L;
 	private static JTextField textField;
+	private JLayeredPane pane;
+	private GridBagConstraints paneConstraints;
+	private static int gridY = 1;
 	
 	public FormulePanel() {
 		
@@ -43,28 +51,107 @@ public class FormulePanel extends JPanel {
 		add(bouton, constraints);
 		
 		
-		JTextArea formulesDeveloppees = new JTextArea();
-		formulesDeveloppees.setEditable(false);
-		formulesDeveloppees.setBackground(new Color(190, 140, 100));
+		pane = new JLayeredPane();
 		constraints.gridx = 0;
 		constraints.gridy = 1;
 		constraints.insets = new Insets(10, 0, 0, 0);
-		add(formulesDeveloppees, constraints);
-		//formulesDeveloppees.viewToModel2D(MouseInfo.getPointerInfo().getLocation());
+		pane.setPreferredSize(new Dimension(1000, 500));
+		pane.setBorder(BorderFactory.createTitledBorder("Arbre"));
+		
+		pane.setLayout(new GridBagLayout());
+		paneConstraints = new GridBagConstraints();
+		paneConstraints.fill = GridBagConstraints.NORTH;
+		add(pane, constraints);
+		
 		
 		bouton.addActionListener(new ActionListener() {
 			
 			@Override
 			public void actionPerformed(ActionEvent e) {
+				
+				for ( Component comp : pane.getComponents() ) {
+					pane.remove(comp);
+				}
+				
+				JTextArea formulesDeveloppees = new JTextArea();
+				formulesDeveloppees.setEditable(false);
 				formulesDeveloppees.setText(textField.getText());
+				formulesDeveloppees.setBounds(5, 30, 990, 30);
+				formulesDeveloppees.setBackground(new Color(190, 140, 100));
+				paneConstraints.gridx = 0;
+				paneConstraints.gridy = 0;
+				paneConstraints.insets = new Insets(10, 0, 0, 0);
+				pane.add(formulesDeveloppees, paneConstraints);
+				
+				addMouseListener(formulesDeveloppees);
+				
+				//pane.updateUI();
+				pane.revalidate();
+				pane.repaint();
+				/*updateUI();
+				repaint();
+				revalidate();
+				doLayout();*/
+				
+				//pane.doLayout();
+				
 				//TODO RESET POINTS
 			}
 		});
+		
 	}
 	
 	
 	public static void setTextInTextField(String text) {
 		textField.setText(text);
+	}
+	
+	
+	
+	private void addMouseListener(JTextArea area) {
+		area.addMouseListener(new MouseListener() {
+			
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void mousePressed(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void mouseExited(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				JTextArea formuleSplitted = new JTextArea();
+				formuleSplitted.setEditable(false);
+				formuleSplitted.setText(textField.getText());
+				formuleSplitted.setBounds(5, 30, 990, 30);
+				formuleSplitted.setBackground(new Color(190, 140, 100));
+				paneConstraints.gridx = 0;
+				paneConstraints.gridy = gridY;
+				gridY++;
+				paneConstraints.insets = new Insets(10, 0, 0, 0);
+				pane.add(formuleSplitted, paneConstraints);
+				
+				pane.revalidate();
+				addMouseListener(formuleSplitted);
+			}
+		});
 	}
 	
 
